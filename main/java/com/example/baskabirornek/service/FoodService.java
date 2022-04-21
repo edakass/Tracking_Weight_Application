56148f44d99d11acca8ab50f12ef93f3c93449ad
Eda Kaş <61595808+edakass@users.noreply.github.com>
2022-04-21 22:28:04 +0300

    Add files via upload

---
+++ main/java/com/example/baskabirornek/service/FoodService.java
@@ -0,0 +1,24 @@
+package com.example.baskabirornek.service;
+
+import com.example.baskabirornek.entity.Food;
+import com.example.baskabirornek.repository.FoodRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class FoodService {
+
+    @Autowired
+    private FoodRepository repository;
+
+    public List<Food> getAllFood(){
+        List<Food> food_list = (List<Food>)repository.findAll();
+        return food_list;
+    }
+    public List<Food> getByKeyword(String keyword) {
+        return repository.findByKeyword(keyword);
+    }
+
+}
