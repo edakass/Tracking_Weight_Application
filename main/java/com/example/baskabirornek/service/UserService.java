3527c0b60c271a8afcf833d21b95eeaf256de1fd
Eda Kaş <61595808+edakass@users.noreply.github.com>
2022-07-03 00:55:28 +0300

    Delete main directory

--- main/java/com/example/baskabirornek/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.example.baskabirornek.service;
-
-import com.example.baskabirornek.entity.User;
-import com.example.baskabirornek.controller.dto.UserRegistrationDto;
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService extends UserDetailsService {
-    User save(UserRegistrationDto registrationDto);
-}
